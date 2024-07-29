243c92f3eb559fad7bddfab19bc392fb86f4deb5
Prince <princetripathi093@gmail.com>
2024-07-30 03:48:14 +0530

    day1

---
+++ day1/find_the_duplicate_number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(nums[abs(nums[i])] < 0) return abs(nums[i]);
+            nums[abs(nums[i])] = -nums[abs(nums[i])];
+        }
+        return 1;
+    }
+};

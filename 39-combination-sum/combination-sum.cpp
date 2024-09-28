42fd1090c9114e49408368e781270e7158fa9055
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2024-09-29 01:05:43 +0530

    Time: 52 ms (14.94%) | Memory: 36.3 MB (20.55%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    void csm(int i, int n, vector<int>&cnd, int tar,vector<int>ds, vector<vector<int>>&ans){
+        if(tar<0) return;
+        if(i == n){
+            if(tar == 0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        // if(tar == 0){
+        //     ans.push_back(ds);
+        //     // return;
+        // }
+        if(cnd[i] <= tar){
+            ds.push_back(cnd[i]);
+            csm(i, n, cnd, tar-cnd[i], ds, ans);
+            ds.pop_back();
+        }
+        csm(i+1, n, cnd, tar, ds, ans);
+
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+        csm(0, candidates.size(), candidates, target, ds, ans);
+        return ans;
+    }
+};

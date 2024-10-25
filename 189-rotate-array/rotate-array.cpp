f011ba5c6de071944ac37f891698d7717cde09e1
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2024-10-26 05:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (81.11%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,13 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n= nums.size();
         k = k%n;
-        vector<int>v;
-        for(int i = 0; i<k; i++){
-            v.push_back(nums[n-k+i]);
-        }
-        for(int i = 0;i<n-k; i++){
-            v.push_back(nums[i]);
-        }
-        nums = v;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };

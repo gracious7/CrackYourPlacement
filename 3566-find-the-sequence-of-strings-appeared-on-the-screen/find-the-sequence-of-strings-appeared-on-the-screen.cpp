af095e7c284ca75d7b3fa1b37d715e369fc11c83
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2024-10-26 22:00:57 +0530

    Time: 73 ms (5.58%) | Memory: 113.5 MB (7.23%) - LeetSync

--- 3566-find-the-sequence-of-strings-appeared-on-the-screen/find-the-sequence-of-strings-appeared-on-the-screen.cpp
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/find-the-sequence-of-strings-appeared-on-the-screen.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<string> stringSequence(string target) {
+        int n = target.size();
+        string s = "";
         vector<string>ans;
-        int n= target.size();
-        string ss = "";
-        for(int i = 0; i<n; i++){
-            char ch = target[i];
-            string tmp = "";
-            for(char c = 'a'; c <= ch; c++){
-                tmp = ss+c;
-                ans.push_back(tmp);
-            }
-            ss = tmp;
+        for(int i = 0;i<n; i++){
+            string ss= s;
+            char c = 'a';
+            ss = s+c;
+            // c++;
+            ans.push_back(ss);
+            while(c < target[i]){
+                c++;
+                ss = s + c;
+                ans.push_back(ss);
 
+            }
+            s = ss;
         }
         return ans;
     }

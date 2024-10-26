f53f85cbed3d6d4d54a789e23d341e150e740bc9
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2024-10-26 19:04:49 +0530

    Time: 67 ms (24.17%) | Memory: 20 MB (14.51%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
 
-    int f(int ind, int n, vector<int>&nums, vector<int>&dp){
-        if(ind == n-1) return 0;
+    // int f(int i, int n, vector<int>&a, vector<int>&dp){
+    //     if(i == n) return 0;
 
-        if(dp[ind] != -1) return dp[ind];
-        int ans = n;
-        for(int i = 1; i<=nums[ind]; i++){
-            if(ind+i < n){
-                int x = 1+f(ind+i, n, nums, dp);
-                ans = min(x, ans);
-            }
-        }
+    // }
 
-        return dp[ind] = ans;
-
-
-    }
 
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, -1);
-        return f(0, n, nums, dp);
-
+        vector<int>dp(n+1, 1e9);
+        dp[0] = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 1; j<=nums[i]; j++){
+                if(i+j < n){
+                    dp[i+j] = min(dp[i+j], dp[i]+1);
+                }
+            }
+        }
+        return dp[n-1];
     }
 };

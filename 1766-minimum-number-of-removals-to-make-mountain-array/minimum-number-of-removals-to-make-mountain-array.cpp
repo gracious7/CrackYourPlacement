816c43cb6a1214a91a95be70ce502050089721bc
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2024-10-30 18:43:31 +0530

    Time: 120 ms (12.12%) | Memory: 15.2 MB (31.84%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& nums) {
-        vector<int> leftDP(nums.size(), 1), rightDP(nums.size(), 1);
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    leftDP[i] = max(leftDP[i], leftDP[j] + 1);
-                }
-            }
-        }
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            for (int j = nums.size() - 1; j > i; --j) {
-                if (nums[i] > nums[j]) {
-                    rightDP[i] = max(rightDP[i], rightDP[j] + 1);
-                }
-            }
-        }
-        int ans = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (leftDP[i] != 1 && rightDP[i] != 1) {
-                ans = max(ans, leftDP[i] + rightDP[i]);
+    int f(int i, int n,  vector<int>&nums, vector<int>&dp){
+        // if(i == n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int mx = 1;
+        for(int j = 0; j<i; j++){
+            if(nums[j] < nums[i]){
+                mx = max(mx, 1+f(j, n, nums, dp));
             }
         }
 
-        return nums.size() - ans + 1;
+        return dp[i] =  mx;
+
+    }
+    int frl(int i, int n, vector<int>& nums, vector<int>& dp) {
+        if(dp[i] != -1) return dp[i];
+
+        int mx = 1;
+        for(int j = i+1; j<n; j++){
+            if(nums[j] < nums[i]){
+                mx = max(mx, 1+frl(j, n, nums, dp));
+            }
+        }
+
+        return dp[i] = mx;
+    }
+
+
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n= nums.size();
+        vector<int>dp(n, -1);
+        vector<int>dp2(n, -1);
+
+        vector<int>l(n);
+        vector<int>r(n);
+
+        for(int i =0; i<n; i++){
+            l[i] = f(i, n, nums, dp);
+        }
+        for(int i =0; i<n; i++){
+            r[i] = frl(i, n, nums, dp2);
+        }
+        int mx = INT_MIN ;
+        int ind = -1;
+         for (int i = 1; i < n - 1; i++) {
+            if (l[i] > 1 && r[i] > 1) {
+                mx = max(mx, l[i] + r[i] - 1);  
+            }
+        }
+
+        return n -mx;
+
+
     }
 };

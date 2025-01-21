bf50a5d68af03e403dda11bcc0ad61c01e7d7ae1
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2025-01-21 23:58:10 +0530

    Time: 7 ms (18.63%) | Memory: 20.6 MB (36.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -13,13 +13,8 @@ public:
     }
 
     int findCircleNum(vector<vector<int>>& mat) {
-        // int m = mat.size();//row
-        // int n = mat[0].size();//col
-
         int n = mat.size();
-
         vector<vector<int>>adj(n);
-
         for(int i =0 ;i<n; i++){
             for(int j = 0; j< n; j++){
                 if(mat[i][j] == 1){
@@ -29,44 +24,14 @@ public:
         }
 
         vector<int>vis(n, 0);
-
         int cnt = 0;//it will count how many times bfs is called
+
         for(int i = 0; i<n; i++){
-
             if(vis[i] == 0){
-
-                //bfs
-                queue<int>q;
-                q.push(i);
-                vis[i] = 1;
-
-                while(!q.empty()){
-                    int node = q.front();
-                    q.pop();
-                    
-                    for(auto x: adj[node]){
-                        if(!vis[x]){
-                            vis[x] = 1;
-                            q.push(x);
-                        }
-                    }
-
-                }
-                //bfs ends
                 cnt++;
-
+                dfs(i, adj, vis);
             }
-
         }
-
         return cnt;
-
-
-
-
-
-
-
-
     }
 };
